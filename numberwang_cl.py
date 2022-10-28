b1dce13b17cb13f925ba7e4aff64e2357f3a71e9
AndrewZ-0 <107640838+AndrewZ-0@users.noreply.github.com>
2022-10-28 13:34:16 +0100

    Update numberwang_cl.py

--- numberwang_cl.py
+++ numberwang_cl.py
@@ -1,4 +1,2 @@
 from random import randint; print("----------------\nNUMBER WANG\n----------------"); wang_found = False
-while wang_found == False and input("guess: "): 
-    if randint(1, 2) == 1: print("THATS NUMBER WANG"); wang_found = True 
-    else: print("that is not number wang")
+while wang_found == False and input("guess: "): print("THATS NUMBER WANG"); wang_found = True if randint(1, 2) == 1 else print("that is not number wang")
