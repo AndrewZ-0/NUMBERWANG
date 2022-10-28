abfb44f6a6f235766602dfc646a05f2c3c1f9055
AndrewZ-0 <107640838+AndrewZ-0@users.noreply.github.com>
2022-10-28 13:18:14 +0100

    Create numberwang_cl.py

---
+++ numberwang_cl.py
@@ -0,0 +1,10 @@
+from random import randint
+print("----------------\nNUMBER WANG\n----------------")
+wang_found = False
+while wang_found == False:
+    guess = input("guess: ")
+    if randint(1, 2) == 1:
+        print("THATS NUMBER WANG")
+        wang_found = True
+    else:
+        print("that is not number wang")
